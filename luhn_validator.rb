9de5fedfd9b2d859fbf444a5a0f65103ff457dcf
bjsalgado <bayardo_salgado@yahoo.com>
2015-03-23 19:10:21 +0800

    Modification of luhn_validator.rb

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,22 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
-  end
+    index = -1
+    double = []
+    sum = nil
+    until nums_a[index].nil? 
+	if index % 2 == 0 
+	   num_double = nums_a[index] * 2 
+	   if num_double > 9
+	     double << (num_double.to_s)[0].to_i + (num_double.to_s)[1].to_i
+	   else
+	     double << num_double
+           end
+	else 	
+	   double << nums_a[index]
+	end
+	index += -1     	
+     end
+    double.each do {|x| sum += x }
+    return sum % 10 == 0 ?  true : false
 end
